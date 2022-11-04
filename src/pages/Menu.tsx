a61bbc440209d2667a652f3e910eccb49aa36cd7
gustavo.carvalho <gustavo.stn@outlook.com.br>
2022-11-03 21:54:30 -0300

    feat(menu): Criando reducers p/ resetar quantidade de produtos no carrinho

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,24 +1,49 @@
-import products from "../server/products-mock.json"
+import mock from "../server/products-mock.json"
 
 import ProductCard from "../components/productCard"
 
 import { IProductInfo } from "../interfaces/productCard-interface"
-import { useSelector } from "react-redux"
-import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { reset } from "../redux/cart/counterSlice"
+import { useEffect, useState } from "react"
 
 function Menu() {
+
+    const dispatch = useDispatch()
     const count =  useSelector((state: any) => state.counter.counter)
-    
+
+    const [products, setProducts] = useState<any[]>([])
+
     useEffect(() => {
-      console.log(count);
-    }, [count])
+        setProducts(mock)
+    
+      return () => {
+        setProducts([])
+      }
+    }, [mock])
     
 
+    function handleResetCart(): void {
+        const allProducts = products.map((product: IProductInfo) => { 
+            product.product_in_card = false 
+            return product
+        })
+        
+        setProducts(allProducts)
+        dispatch(reset())
+    }
+
     return (
-        <div className="w-full flex flex-col justify-center align-center">
-            <h2 className="w-full flex justify-center align-center text-white font-bold text-3xl py-8">
-                Produtos no carrinho {count}
-            </h2>
+        <div className="container-sm flex flex-col justify-center align-center">
+            <div className="inline-flex align-center justify-between mb-8">
+                <h2 className="flex justify-center items-center text-white font-bold text-3xl">
+                    Produtos no carrinho {count}
+                </h2>
+                <button
+                    className="mt-2 w-full p-2 rounded-md bg-transparent border border-red-600 text-white w-40 max-h-10 hover:bg-red-600"
+                    onClick={_ => handleResetCart()}
+                >Limpar carrinho</button>
+            </div>
             <div className="grid grid-cols-3 gap-4 justify-items-center">
                 {
                     products.map((product: IProductInfo) => {
--- src/redux/cart/counterSlice.ts
+++ src/redux/cart/counterSlice.ts
@@ -11,10 +11,16 @@ export const counterSlice = createSlice({
         },
         decrement: (state) => {
             state.counter -= 1
+        },
+        reset: (state) => {
+            state.counter = 0
+        },
+        incrementByAmount: (state, qty) => {
+            state.counter =+ qty.payload
         }
     }
 })
 
-export const { increment, decrement } = counterSlice.actions
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions
 
 export default counterSlice.reducer
